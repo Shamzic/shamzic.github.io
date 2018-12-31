46b428fdcd017d71bb128bb11cd646045ff075f9
Shamzic <shamery@free.fr>
2018-12-31 03:41:05 +0100

    site v2 reactJS + react mdl

---
+++ precache-manifest.65ae0df9e4fee87dc55b97a02a1a9988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abe0a5faf4fececeabba",
+    "url": "/static/js/main.abe0a5fa.chunk.js"
+  },
+  {
+    "revision": "b0e79f9e3f58c92c6816",
+    "url": "/static/js/1.b0e79f9e.chunk.js"
+  },
+  {
+    "revision": "abe0a5faf4fececeabba",
+    "url": "/static/css/main.29929df2.chunk.css"
+  },
+  {
+    "revision": "b0e79f9e3f58c92c6816",
+    "url": "/static/css/1.d7b19d36.chunk.css"
+  },
+  {
+    "revision": "b56452319d11872dbf039550f9b08d5c",
+    "url": "/index.html"
+  }
+];
